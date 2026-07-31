ee6c1fb3c3c83aae9f663ba8216f6d498d8608b9
lilouelle2023-hub <lilouelle2023@gmail.com>
2026-07-31 17:40:48 +0200

    fix(i18n): add EN page for technologies-datacenter-ia-2026-800v-liquide-smr

---
+++ frontend-nextjs/pages/en/blog/datacenter-technologies-2026-800v-liquid-smr.tsx
@@ -0,0 +1,9 @@
+export { default } from '../../blog/technologies-datacenter-ia-2026-800v-liquide-smr';
+
+export async function getStaticProps() {
+  return {
+    props: {
+      initialLanguage: 'en'
+    }
+  };
+}
