7de7db948d88fb9e5b2f4d5c956b1f9a44672135
lilouelle2023-hub <lilouelle2023@gmail.com>
2026-07-31 17:27:24 +0200

    fix(i18n): add EN page for clouds-chinois-europe-datacenter-investissement

---
+++ frontend-nextjs/pages/en/blog/chinese-clouds-europe-datacenter-investment.tsx
@@ -0,0 +1,9 @@
+export { default } from '../../blog/clouds-chinois-europe-datacenter-investissement';
+
+export async function getStaticProps() {
+  return {
+    props: {
+      initialLanguage: 'en'
+    }
+  };
+}
