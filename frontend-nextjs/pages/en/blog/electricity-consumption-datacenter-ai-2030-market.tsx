c92cf017db507c6a70049a8ccc4299eba2f0528f
lilouelle2023-hub <lilouelle2023@gmail.com>
2026-07-31 17:40:49 +0200

    fix(i18n): add EN page for consommation-electrique-datacenter-ia-2030-marche

---
+++ frontend-nextjs/pages/en/blog/electricity-consumption-datacenter-ai-2030-market.tsx
@@ -0,0 +1,9 @@
+export { default } from '../../blog/consommation-electrique-datacenter-ia-2030-marche';
+
+export async function getStaticProps() {
+  return {
+    props: {
+      initialLanguage: 'en'
+    }
+  };
+}
